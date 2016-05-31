597a97f63067a39121cae49f1897e2c0e9acadd8
Ingmar Delsink <ingmardelsink@hotmail.com>
2016-05-31 19:56:35 +0200

    add yes parameter to script

--- install_latex.sh
+++ install_latex.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Install LaTeX and needed deps for the documentation
 
-VERSION="0.1.0"
+VERSION="0.1.1"
 
 echo "-- Installing LaTeX dependencies for this project v$VERSION"
 echo
 
+YES=""
 # Get package manager
 DNF_CMD=$(which dnf)
 APT_GET_CMD=$(which apt-get)
@@ -57,14 +58,19 @@ DEB_PACKAGE_NAMES=(
     make
 )
 
+# YES check
+if [[ $1 == "YES" ]]; then
+    YES="-y"
+fi
+
 if [[ ! -z $DNF_CMD ]]; then
     echo "Installing with DNF"
     sudo dnf check-update
-    sudo dnf install ${DNF_PACKAGE_NAMES[@]}
+    sudo dnf install ${DNF_PACKAGE_NAMES[@]} ${YES}
 elif [[ ! -z $APT_GET_CMD ]]; then
     echo "Installing with APT"
     sudo apt-get update
-    sudo apt-get install ${DEB_PACKAGE_NAMES[@]} --no-install-recommends # No recommended docs of 500+ MB!
+    sudo apt-get install ${DEB_PACKAGE_NAMES[@]} ${YES} --no-install-recommends # No recommended docs of 500+ MB!
 else
     echo "package manager not supported"
     exit 1;
